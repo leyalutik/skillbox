2f75a735b692c6f0ee47ee6c2b08239143915b41
leya0lutik <>
2023-04-08 13:04:20 +0000

    111

--- pushtogit.sh
+++ pushtogit.sh
@@ -2,5 +2,5 @@ git add .
 git config --global user.email "ludapevek@gmail.com"
 git config --global user.name "leyalutik"
 git commit -m "111"
-git remote add origin https://github.com/leyalutik/Skillbox_all_tasks.git
+git remote add origin https://github.com/leyalutik/Skillbox.git
 git push -u -f origin master
