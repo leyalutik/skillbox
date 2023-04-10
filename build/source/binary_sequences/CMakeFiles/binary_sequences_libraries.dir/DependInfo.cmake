c9cee7a59947d17fe3652aeac9eb8e24885e4e64
leya0lutik <>
2023-04-10 11:57:53 +0000

    tickle commit

---
+++ build/source/binary_sequences/CMakeFiles/binary_sequences_libraries.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/Skillbox/source/binary_sequences/binary_sequences.cpp" "source/binary_sequences/CMakeFiles/binary_sequences_libraries.dir/binary_sequences.cpp.o" "gcc" "source/binary_sequences/CMakeFiles/binary_sequences_libraries.dir/binary_sequences.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/source/sort_functions/CMakeFiles/sort_functions_libraries.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/Skillbox/source/sort_functions/base_functions.cpp" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/base_functions.cpp.o" "gcc" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/base_functions.cpp.o.d"
+  "/home/runner/Skillbox/source/sort_functions/bubble_sort.cpp" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/bubble_sort.cpp.o" "gcc" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/bubble_sort.cpp.o.d"
+  "/home/runner/Skillbox/source/sort_functions/heap_sort.cpp" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/heap_sort.cpp.o" "gcc" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/heap_sort.cpp.o.d"
+  "/home/runner/Skillbox/source/sort_functions/insert_sort.cpp" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/insert_sort.cpp.o" "gcc" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/insert_sort.cpp.o.d"
+  "/home/runner/Skillbox/source/sort_functions/merge_sort.cpp" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/merge_sort.cpp.o" "gcc" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/merge_sort.cpp.o.d"
+  "/home/runner/Skillbox/source/sort_functions/quick_sort.cpp" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/quick_sort.cpp.o" "gcc" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/quick_sort.cpp.o.d"
+  "/home/runner/Skillbox/source/sort_functions/selection_sort.cpp" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/selection_sort.cpp.o" "gcc" "source/sort_functions/CMakeFiles/sort_functions_libraries.dir/selection_sort.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
