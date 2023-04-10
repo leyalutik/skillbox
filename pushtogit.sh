f2302cca594b588c52691951d9c06e16fa26b698
leya0lutik <>
2023-04-10 12:03:30 +0000

    help commits

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,6 +1,11 @@
 git add .
 git config --global user.email "ludapevek@gmail.com"
 git config --global user.name "leyalutik"
+#for linux users
+git config --global core.autocrlf input
+git config --global core.safecrlf warn
+#for output unicode text
+git config --global core.quotepath off
 git commit -m "111"
 git remote add origin https://github.com/leyalutik/Skillbox.git
 git push -u -f origin master
