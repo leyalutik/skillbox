dbd854c95337a79bced960cdfb1640e0b9feb183
leyalutik <60062618+leyalutik@users.noreply.github.com>
2023-04-12 17:12:54 +0300

    Delete pushtogit.sh

--- pushtogit.sh
+++
@@ -1,6 +0,0 @@
-git add .
-git config --global user.email "ludapevek@gmail.com"
-git config --global user.name "leyalutik"
-git commit -m "111"
-git remote add origin https://github.com/leyalutik/Skillbox.git
-git push -u -f origin master
