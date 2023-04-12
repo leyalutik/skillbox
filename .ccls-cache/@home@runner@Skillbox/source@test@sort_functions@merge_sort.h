3a9f4e0b5f1335972754b1f44feb8d7ccd175cd9
leyalutik <60062618+leyalutik@users.noreply.github.com>
2023-04-12 16:36:33 +0300

    Delete .ccls-cache directory

--- .ccls-cache/@home@runner@Skillbox/source@test@sort_functions@merge_sort.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<iostream>
-
-void merge(float* left, const size_t Nl, float* right, const size_t Nr, const bool increasing, float* result, int32_t& operations);
-void merge_sort(float* m , const size_t N,const bool increasing, int32_t& operations);
