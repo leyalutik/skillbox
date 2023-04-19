203b2574c42b3a0bedd973598751476fd0c181f1
leyalutik <ludapevek@gmail.com>
2023-04-19 11:49:15 +0000

    proba

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,6 +1,11 @@
 git add .
 git config --global user.email "ludapevek@gmail.com"
 git config --global user.name "leyalutik"
-git commit -m "111"
+echo Enter the name of commit
+read commt
+git commit -m "$commt"
+git status
+echo enter the name of branch to push:
+read brnch
 git remote add origin https://github.com/leyalutik/Skillbox.git
-git push -u -f origin master
+git push origin $brnch
