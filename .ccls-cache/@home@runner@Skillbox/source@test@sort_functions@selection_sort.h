0f31b412fb7928cec24731dda8d5ff0e5575c1d4
leyalutik <60062618+leyalutik@users.noreply.github.com>
2023-04-21 15:47:38 +0300

    Delete .ccls-cache directory

--- .ccls-cache/@home@runner@Skillbox/source@test@sort_functions@selection_sort.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<vector>
-#include<iostream>
-
-void selection_sort(float* m, const size_t N, const bool increasing, int32_t& operations);
-void selection_sort(std::vector<float>& v, const bool increasing, int32_t& operations);
