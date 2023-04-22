a8043747a4adbbf88b04ce0bf5d672e458131df3
leyalutik <ludapevek@gmail.com>
2023-04-22 14:45:29 +0000

    no resious

--- pushtogit.sh
+++ pushtogit.sh
@@ -7,5 +7,19 @@ git commit -m "$commt"
 git status
 echo  ENTER the name of BRANCH to push:
 read brnch
+git checkout -b $brnch
+git tag "tag_$brnch"
+git checkout -b "$brnch_remote"
+rm -r -f build
+rm -r -f HowToGit.txt
+rm -r -f cmakerun.sh
+rm -r -f 13_Tasks
+git add .
+git commit -m "commit to send to github"
+ls
+echo Has been deleted enough files? (To terminate \"ctrl+Z\")
+read p
 git remote add origin https://github.com/leyalutik/Skillbox.git
 git push origin $brnch
+git branch -d "$brnch_remote"
+
