b198be011474cbfe3c1abbfbe290e274d8df4736
leyalutik <ludapevek@gmail.com>
2023-04-23 14:56:34 +0000

    save info

--- main.cpp
+++ main.cpp
@@ -3,15 +3,81 @@
 #include <cassert>
 #include<vector>
 
-bool is_positive(const int32_t number)
+//add element to the end -> delete element from the beginning
+
+//Fix size and access to any item of matrix
+//can use moving index
+//shift - difference between current index and original index (in the starting position)
+//capacity - max size of store
+size_t moving_index(const size_t index_original, const size_t shift, const size_t capacity);
+void increment_shift(const size_t capacity,size_t& shift);
+void add_element(const int32_t value, size_t& shift, const size_t capacity, std::vector<int32_t>& v);
+void display(const std::vector<int32_t>& v,const size_t shift, const size_t capacity);
+void input(int32_t& value);
+
+int main()
 {
-	return (number >= 0);
+	size_t capacity = 20;
+	std::vector<int32_t> v;
+	v.reserve(capacity);
+
+	int32_t value = 0;
+	size_t shift = 0; // difference current and  original indices
+	while(value != -2) //Terminate the program
+		{
+			while(value != -1 && value != -2) //display vector
+				{	
+					std::cout << "Input the value of store :\n";
+					input(value);
+					if(value != -1 && value != -2)
+					{
+						add_element(value, shift, capacity, v);
+					}
+			
+				}	
+			std::cout << "Current store:\n";
+			display(v,shift,capacity);
+			std::cout << "\n";
+			value = (value == -1 ? 0 : value) ;
+		}
+	
+	return 0;
+}
+//--------------FUNCTIONS-----------------
+void increment_shift(const size_t capacity, size_t& shift)
+{
+	++shift;
+	shift %= capacity;
 }
 
-template <class T>
-void input(T& elem) // T standart types 
+size_t moving_index(const size_t index_original, const size_t shift, const size_t capacity)
 {
-	std::cin >> elem;
+	size_t result = (index_original + shift)% capacity;
+	return result;
+}
+
+void add_element(const int32_t value,  size_t& shift, const size_t capacity, std::vector<int>& v)
+{
+	if(v.size() > capacity)
+		{
+			std::cout << "Illegal value of capacity.\n The size vector is more than its capacity.\n";
+		return;
+		}
+	else if(v.size() < capacity)
+	{
+		v.push_back(value);
+	}
+	else if (v.size() == capacity)
+		{
+			v[moving_index(0,shift,capacity)] = value;
+			increment_shift(capacity,shift);
+		}
+}
+
+
+void input(int32_t& value) 
+{
+	std::cin >> value;
 		while (!std::cin ) {
 			if (std::cin.bad()) //поток повреждён; Отправляем отчет об ошибке
 								//внешней программе
@@ -28,69 +94,29 @@ void input(T& elem) // T standart types
 			std::cin.ignore(32767, '\n'); // delete garbage
 
 			std::cout << "Illegal value. Input again:\n";
-			std::cin >> elem;
+			std::cin >> value;
 		}
 
 }
-template <class T>
-std::ostream& operator<< (std::ostream& os, const std::vector<T> v)
+
+void display(const std::vector<int32_t>& v,const size_t shift, const size_t capacity)
 {
-	os << "{";
+	size_t shift_temp = shift;
+	if(v.size() < capacity)
+	{
+		shift_temp = 0;
+	}
+	std::cout  << "{";
 	bool first = true;
-	for(const auto& elem : v)
+	for(size_t i= (shift_temp)%capacity, counter = 0; counter<v.size(); ++counter, i = (i+1)%capacity)
 		{
 			if(!first)
 			{
-				os << ", "; 
+				std::cout << ", "; 
 			}
 			
 			first = false;
-			os << elem;
+			std::cout << v[i];
 		}
-	os << "}";
-	return os;
+	std::cout  << "}";
 }
-
-
-void remove_value(const int32_t value, std::vector<int32_t>& v)
-{
-	int k = 0;
-	for(size_t i=0; i<v.size(); ++i)
-		{
-			if(v[i] != value)
-			{
-				v[k] = v[i];
-				++k;
-			}
-		}
-	v.resize(k);
-}
-
-//--------------------------MAIN-------
-
-int main()
-{
-	
-std::vector<float> prices {2.5, 4.25, 3.0, 10.0};
-
-// Вектор покупок: {1, 1, 0, 3}, то есть два товара по индексу 1 и по одному — индексов 0 и 3.
-
-std::vector<int> items {1, 1, 0, 3};
-float sum = 0.;
-for(size_t i=0; i< items.size(); ++i)
-	{
-		if(items[i] >= prices.size() || items[i] < 0)
-		{
-			std::cout << "Illegal value of vector \"items\" elemens.\nTerminate program.\n";
-			return 0;
-		}
-		sum += prices[items[i]];
-	}
-	
-std::cout << "Суммарная стоимость будет равна " << sum << "\n"; 
-	
-	return 0;
-}
-
-
-
