17dfaf9a22721975094d6b33f264d9c47421f24d
leyalutik <ludapevek@gmail.com>
2023-05-04 13:54:07 +0000

    save

--- .ccls-cache/@home@runner@Skillbox/main.cpp
+++ .ccls-cache/@home@runner@Skillbox/main.cpp
@@ -2,30 +2,27 @@
 #include <iostream>
 #include <cassert>
 #include<vector>
-
+#include "source/output.h"
 //--------------------------MAIN-------
 
 int main()
 {
-	
-std::vector<std::vector<int>> chairs =   {{1,1,1,1,1,1},{1,1,1,1,1,1}};
-	
-std::vector<std::vector<int>> plates = {{3,2,2,2,2,2},{3,2,2,2,2,2}};
+	char grid[3][3];
+	for(size_t i=0; i<9; ++i)
+		{//Initialization grid
+			grid[i/3][i%3] = '.';
+		}
+	output(grid);
 
-	std::vector<std::vector<int>>  tableware = {{4,3,3,3,3,3},{4,3,3,3,3,3}};
-
-//Woman came with child. 
-	std::cout << "Woman with child had " << ++chairs[0][4] << " chairs.\n";
-	//Somebody stole a spoon. 
-std::cout << "After stealing a spoon one person had " <<	--tableware[1][2] << " tablewares.\n";;
-	//vip shared a spoon. And waiter took one plate.
-	std::cout << "After sharing a spoon Vip had " <<	----tableware[0][0] << " tablewares and after taking one plate by waiter he had  " << --plates[0][0] << " plates.\n";;
-std::cout << "Vip shared a spoon with one person. And person than had " <<	++tableware[1][2] << " tablewares.\n";
-
-	
+	char gamers[2] = {'X','O'};
+	bool turn_X = true;
+	while()
+		{
+			std::cout << "Turn for " << (turn_X ? 'X' : 'O') << ":\nEnter x,y of grid where 1 ";
+		}
 	
 	return 0;
 }
 
-
+//Нет автоматической инициализация массива имволов
 
---
+++ build/source/CMakeFiles/source_libraries.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source_libraries.dir/output.cpp.o"
+  "CMakeFiles/source_libraries.dir/output.cpp.o.d"
+  "libsource_libraries.a"
+  "libsource_libraries.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/source_libraries.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
