333f404e0adee8fb07766977df55fd3da78e2eb6
leyalutik <60062618+leyalutik@users.noreply.github.com>
2023-05-05 16:37:35 +0300

    Delete .ccls-cache directory

--- .ccls-cache/@home@runner@Skillbox/build@test_cases.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_CASES_H 
- #define TEST_CASES_H
-void test_cases();
-void test_case1();
-void test_case2();
-#endif
--- .ccls-cache/@home@runner@Skillbox/output.h
+++
@@ -1,9 +0,0 @@
-#ifndef OUTPUT_H 
-#define OUTPUT_H 1
-
-#include<string>
-
-void output(const std::string& grid);
-
-
-#endif
