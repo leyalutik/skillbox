89f8a01a48d272f369134d861b675ef6866f32d3
leyalutik <ludapevek@gmail.com>
2023-05-13 11:21:48 +0000

    save

--- main.cpp
+++ main.cpp
@@ -4,24 +4,68 @@
 #include<vector>
 
 //--------------------------MAIN-------
-vold input(float& value);
+void input(float& value);
+void input(std::vector<float>& v);
+void display(const std::vector<float>& v);
 int main()
 {
-	for (int ch = -128; ch < 128; ++ch)
-		{
-			char sym = ch;
-			std::cout << " Code = " << ch << " sym = " <<  sym << " ;";
-			if(ch % 2 == 0)
-			{
-				std::cout << std::endl;
-			}
-		}
-	
+	std::cout << "Enter value: ";
+	float value = 0;
+	input(value);
+	std::cout << value << "\n";
+	std::vector<float> v = {1,2,3};
+	std::cout << "Size = " << v.size();
+	v.clear();
+	std::cout << "Size = " << v.size();
 	return 0;
 }
+//-------------MAIN-----------------
 
-vold input(float& value)
+void display(const std::vector<float>& v);
+
+
+void input(std::vector<float> v)
 {
+	v.clear();
+
+	for(size_t i = 0; i<v.size(); ++i)
+		{
+			float value = 0;
+			input(value);
+			v.push_back(value);
+		}
+}
+
+
+
+void input(float& value)
+{
+	std::cin >> value;
+	while(!std::cin)
+	{
+		if(std::cin.bad())  //поток повреждён; Отправляем отчет об ошибке внешней программе
+		{
+			std::cout << "In function stream cin was corrupted";
+			std::getchar();
+			return ;
+		}
+		if(std::cin.eof())
+		{
+			std::cout << "The input stream is overloaded.\n";
+			std::getchar();
+			return ;
+		
+			//Больше никаких входных данных
+			//Последовательность символов правильно должна оканчиваться именно так
+		}
+		if(std::cin.fail()) // поток столкнулся с чем-то неожиданным
+		{
+			std::cin.clear();// make ready for more input
+			std::cin.ignore(32767, '\n'); // очищаем поток от мусора
+			std::cin >> value;
+		}
+	}
+	
 	
 }
 
