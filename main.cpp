98b6e57c40d9be7c3b773cfc81e8c33fbbd3945c
leyalutik <ludapevek@gmail.com>
2023-05-13 11:02:30 +0000

    save

--- main.cpp
+++ main.cpp
@@ -4,11 +4,24 @@
 #include<vector>
 
 //--------------------------MAIN-------
-
+vold input(float& value);
 int main()
 {
+	for (int ch = -128; ch < 128; ++ch)
+		{
+			char sym = ch;
+			std::cout << " Code = " << ch << " sym = " <<  sym << " ;";
+			if(ch % 2 == 0)
+			{
+				std::cout << std::endl;
+			}
+		}
+	
 	return 0;
 }
 
-
+vold input(float& value)
+{
+	
+}
 
