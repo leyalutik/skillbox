bfe5814b78d83104acab8ac943de52c9848f493c
leyalutik <60062618+leyalutik@users.noreply.github.com>
2023-05-14 16:54:31 +0300

    Delete .ccls-cache directory

--- .ccls-cache/@home@runner@Skillbox/source@sort_functions@bubble_sort.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<vector>
-#include<iostream>
-
-void bubble_sort(float* m, const size_t N, const bool increasing, int32_t& operations);
-void bubble_sort(std::vector<float>& v, const bool increasing, int32_t& operations);
