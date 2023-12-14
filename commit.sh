55f4b76e6b1ac08367a1047a9800d6ed25e418dc
leyalutik <ludapevek@gmail.com>
2023-12-14 17:39:43 +0300

    Task 14.5

--- commit.sh
+++
@@ -1,7 +0,0 @@
-
-git add .
-git config --global user.email "ludapevek@gmail.com"
-git config --global user.name "leyalutik"
-echo Input description for commit :
-read commit
-git commit -m "$commit"
