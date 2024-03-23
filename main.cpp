7eda5dbe0be20898a6d35c4615b768287e6b306b
leyalutik <ludapevek@gmail.com>
2024-03-23 13:17:44 +0300

    check

--- main.cpp
+++ main.cpp
@@ -7,75 +7,72 @@
 
 
 
-int max_accumulator(const std::vector<int>& seq);
+void find_sum_pair(const int REZ, const std::vector<int>& sorted_list);
+void sort(std::vector<int>& v);
 void test();
 
 
 int main()
 {
-//	test();
+	test();
+	std::vector<int> v = {1,2,3,4,5,6,7};
+	sort(v);
+	find_sum_pair(9,v);
 
-	std::vector<int> v = {1,2,3};
-	max_accumulator(v);
+	return 0;
 }
 
-int max_accumulator(const std::vector<int>& seq)
-{
-	if(seq.size() <2)
-	{
-		std::cerr << "Incorrect input data.\n";
-		return -1;
-	}
-int N = seq.size();
-int left=0, right=1;
-int max = seq[0]+seq[1];
 
-for(int i=0; i<N-1; ++i)
+
+void find_sum_pair(const int REZ, const std::vector<int>& sorted_list)
 {
-	int sum = seq[i];
-	for(int j=i+1;j<N;++j)
+	if(sorted_list.size() < 2)
 	{
-		sum +=seq[j];
-		if(sum > max)
+		std::cerr << "Incorrect number of elements in sequence.\n";
+		return;
+	}
+	
+	int i=0,j=0;
+	while(sorted_list[i]+sorted_list[j] < REZ)
+	{
+		++j;
+	}
+
+	while(i<=j)
+	{
+		while(sorted_list[i]+sorted_list[j] < REZ)
 		{
-			max = sum;
-			left = i;
-			right = j;
+			++i;
+
 		}
+		if(sorted_list[i]+sorted_list[j] == REZ)
+		{
+			std::cout << "\nThe given number = " << REZ << "\nNum1 = " <<sorted_list[i] << " Num2 = " <<sorted_list [j] << "\n"; 
+		}
+
+
+		--j;
 	}
 }
-	std::cout << "Left index = " << left << "\nRight index = " << right <<"\nmax sum = " << max << "\n";;
-	return max;
-
+void sort(std::vector<int>& v)
+{
+	std::sort(v.begin(), v.end());
 }
 
 void test()
 {
-	//input data - right answer
-	std::vector<std::vector<int>> input_data = {
-		{1,2,3,4,5},
-		{-10,5,6,7},
-		{1,2,3,-6,1,2,4},
-		{1,2,3,-6,1,2,3},
-		{1,2,3,-10},
-		{1,1,1,-10,5,6,7,-10,1,1,1},
-		{1,2,3,-10,1,1,1,-10,1,1,1},
-		{1,2,3,-10,1,1,1,-10,1,2,3},
-
+	std::vector<std::vector<int>> v = {
+		{0},
+		{-1,-2,-3,-4,5,6,7},//-4
+		{0,5,6,2,3,6,1,-1},//1
+		{6,7,-1,8,2}, //1
+		{4,3,6,2,4,8,1,10},//18
+		{3,8,2,3,9,-1,0}, //9
+		{0,-1,-2,-3,1,2,3} //0
 	};
-	std::vector<int> right_answer = {15,18,7,6,6,18,6,6};
-
-	//check function
-	for(int i=0; i<input_data.size(); ++i)
+	std::vector<int> REZ =  {1,-4,1,1,18,9,0};
+	for(int i=0; i< v.size(); ++i)
 	{
-
-
-		assert(max_accumulator(input_data[i]) == right_answer[i]);
-		
+		find_sum_pair(REZ[i],v[i]);
 	}
-
-	std::cerr << "TEST - OK.";
-
 }
-
-
