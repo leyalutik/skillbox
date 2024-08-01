548919f61c42a4d23b2678cfa00f91faeccb8b18
leyalutik <ludapevek@gmail.com>
2024-08-02 00:11:41 +0300

    commit with commit.sh

--- main.cpp
+++ main.cpp
@@ -1,45 +1,192 @@
-#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cassert>
 
+bool is_line2_in_line1(const char* line1, const char* line2);
 
-
-void swap(int& i, int& j)
-{
-	int temp = i;
-	i = j;
-	j = temp;
-}
-
-void reverse_massive(const int& N, int* p)
-{
-	int i=0, j=9;
-	while(i<j)
-	{
-		swap(*(p+i),*(p+j));
-		++i;
-		--j;
-	}
-}
-
-
-void print(const int& N, int* p)
-{
-
-	std::cout << std::endl;
-	for(size_t i=0; i<N; ++i)
-	{
-		std::cout << *(p+i);
-	}
-	std::cout << std::endl;
-}
-
+void test();
 
 int main()
 {
+	char a[] = "sdf";
+	char b[] = "adf";
 
-	int a[10] = {0,1,2,3,4,5,6,7,8,9};
-	reverse_massive(10,a);
-	print(10,a);
+	printf ("\n%d\n",is_line2_in_line1(a,b));
 
 	return 0;
 
 }
+
+bool is_line2_in_line1(const char* line1, const char* line2)
+{
+	if(std::strlen(line1) == 0 || std::strlen(line2) == 0 
+			|| std::strlen(line2) > std::strlen(line1)
+			|| line2[std::strlen(line2)] != '\0' || line1[std::strlen(line1)] != '\0'
+	  )
+	{
+		return false;
+	}
+
+	int k=0;
+	for(size_t i=0; i<std::strlen(line1); ++i)
+	{
+		if(line1[i] == line2[k++])
+		{
+			if(k == std::strlen(line2))
+			{
+				return true;
+			}
+		}
+		else
+		{
+			if(i > (std::strlen(line1)-std::strlen(line2)))
+			{
+				return false;
+			}
+			k = 0;
+			
+		}
+	
+	}
+	return false;
+
+}
+
+void test()
+{
+	{
+	const char s1[] = "\0";
+	const char s2[] = "\0";
+
+	assert(is_line2_in_line1(s1,s2) == false);
+	}
+	{
+	const char s1[] = "f\0";
+	const char s2[] = "\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  false);
+	}
+
+	{
+	const char s1[] = "\0";
+	const char s2[] = "f\0";
+
+	assert(is_line2_in_line1(s1,s2) == false);
+	}
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "s\0";
+
+	assert(is_line2_in_line1(s1,s2) == true);
+	}
+
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "d\0";
+
+	assert(is_line2_in_line1(s1,s2) == true);
+	}
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "f\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  true);
+	}
+
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "sdfs\0";
+
+	assert(is_line2_in_line1(s1,s2) == false);
+	}
+
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "sd\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  true);
+	}
+
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "df\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  true);
+	}
+
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "sf\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  false);
+	}
+
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "fa\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  false);
+	}
+
+	{
+	const char s1[] = "sdf\0";
+	const char s2[] = "as\0";
+
+	assert(is_line2_in_line1(s1,s2) == false);
+	}
+
+	{
+	const char s1[] = "sdfsd\0";
+	const char s2[] = "fsd\0";
+
+	assert(is_line2_in_line1(s1,s2) == true);
+	}
+	{
+	const char s1[] = "sdfsdf\0";
+	const char s2[] = "sdf\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  true);
+	}
+
+	{
+	const char s1[] = "sdfsdf\0";
+	const char s2[] = "dfs\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  true);
+	}
+
+	{
+	const char s1[] = "sdfsdf\0";
+	const char s2[] = "sdfsdf\0";
+
+	assert(is_line2_in_line1(s1,s2) == true);
+	}
+
+	{
+	const char s1[] = "sdrfsdf\0";
+	const char s2[] = "sdf\0";
+
+	assert(is_line2_in_line1(s1,s2) ==  true);
+	}
+
+{
+	const char s1[] = "sdf";
+	const char s2[] = "as\0";
+
+	assert(is_line2_in_line1(s1,s2) == false);
+	}
+
+{
+	const char s1[] = "sdf\0";
+	const char s2[] = "as\0";
+
+	assert(is_line2_in_line1(s1,s2) == false);
+	}
+
+
+
+
+
+}
+
+
