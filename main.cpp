c76670a5c4b0267ab0f8d4364c89fe5cc74592a5
leyalutik <ludapevek@gmail.com>
2024-10-15 12:18:47 +0300

    check_function_write_data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 
 #include<iostream>
-
+#include<iomanip>
+#include<vector>
+#include<string>
+/*
 enum class  RoomType
 {
 	bedroom = 1,
@@ -17,107 +20,68 @@ enum class BuildingType
 	sauna = 3;
 };
 
+*/
+
 
 struct Room
 {
-	char* room_names[3] = {"Bedroom", "Kitchen", "Bathroom", "Children\'s room","Dining room" };
-int32_t type_room;
-	int32_t S=0;
-
 	void write_data();
 	bool check_data();
+	void print_data();
+	void evaluate_area();
 
 
-void print_room(const BuildingType& buiding_type);
+	std::vector<std::string> room_names = {"Bedroom", "Kitchen", "Bathroom", "Children\'s room","Dining room" };
+int32_t type_room = 0;
+	int32_t S=0;
+
+	
 };
 
 struct Floor
 {
 
+	void write_data();
+	bool check_data();
+	void print_data();
+	void evaluate_area();
+
 	int32_t number_rooms=0;
 	int32_t height=0;
-	void write_data();
-	bool check();
-
 	int32_t S=0;
 	std::vector<Room> rooms;
-	void evaluate_area();
-};
-
+	
+	};
 
 struct House
 {
+	void write_data();
+	bool check_data();
+	void print_data();
+	void evaluate_area();
+
+
 	int32_t number_floors=0;
 	bool has_pipe_furnace=0;
 	std::vector<Floor> floors;
 
-	void write_data();
-	bool check_data();
-	void evaluate_S();
 
-	int32_t S = 0;
-	void evaluate_area();
+	double  S = 0;
 
 };
 
 struct Building
 {
 
-void print_data()
-{
-	std::cout << std::setw(10) << "Additional building: " << building_names[type_building]; << "\n";
+void print_data();
+bool check_data();
+void write_data();
 
-	std::cout << "There is " << (has_pipe_furnace ? "a " : "no ") << "furnace with a pipe.\n":
-	std::cout << "Area: " << S << "\n":
-}
+double  evaluate_area();
 
-char* building_names[3] = {"Garage", "Shed", "Sauna"};
-int32_t type_building;
-
-bool check()
-{
-	if (building_type != 0 AND  building_type != 1 || building_type != 2) 
-	{
-		return 0;
-
-	}
-	if (S < 0)
-	{
-		return 0;
-	}
-	if(has_pipe_furnace != true AND has_pipe_furnace != false)
-	{
-		return 0;
-	}
-	return 1;
-}
-
-void write_data()
-{
-	while(true)
-	{
-		std::cout << "Input buiding type number:\n\n";
-
-
-		std::cin >> building_type;
-		std::cout << "Input whether the house hase furnace with a pipe. Yes(1) / No(0)\n";
-		std::cout << "Input the size of the area:\n";
-		std::cin >> has_pipe_furnace;
-
-		if(check())
-		{
-
-			break;
-		}
-		else
-		{
-			std::cout << "Invalid data. Try again.\n";
-			std::cin.ignore(1000,'\n');
-			std::cin.good();
-		}
-	}
-}
-int32_t S=0;
+std::vector<std::string> building_names = {"Garage", "Shed", "Sauna"};
+int32_t building_type = 0;
+double S = 0.0;
 bool has_pipe_furnace = 0;
 
 };
@@ -126,19 +90,20 @@ struct Section
 	void write_data(); //number_add_buildings and reserve capacity for vector
 	bool check_data();
 	void print_data();
+	void evaluate_area();
+
 	int32_t number_add_buildings;
 	std::vector<Building> additional_buildings;
 	House house;
 	
 	int32_t S = 0;
-	void evaluate_area();
 
 };
 
 
 struct Village
 {
-	void write_data()
+	/*void write_data()
 	{
 		while(true)
 		{
@@ -154,48 +119,120 @@ struct Village
 				}
 			}
 		}
-	}
+	}*/
 	bool check_data();
 	void print_data();
+	void evaluate_area();
 
 	int32_t number_sections=0;
-	vector<Section> sections;
+	std::vector<Section> sections;
 	int32_t S = 0;
-	void evaluate_area();
 };
 
 
-
+//----------------------MAIN START---------------
 int main()
 {
-	Village village;
-	village.write_data();
-	village.evaluate_area();
-	village.print_data();
+	while(true)
+	{
+Building b;
+b.write_data();
+b.print_data();
+	}
+//	Village village;
+//	village.write_data();
+//	village.evaluate_area();
+//	village.print_data();
 
 	return 0;
 }
 
 
-//---------------------------
-void Room::print_room(const BuildingType& buiding_type)
+//---------------------MAIN END------------------
+
+//-----ROOM------------------------
+/*struct Room
 {
-	switch(buiding_type)
+	void write_data();
+	bool check_data();
+	void print_data();
+	void evaluate_area();};
+
+
+	char* room_names = {"Bedroom", "Kitchen", "Bathroom", "Children\'s room","Dining room" };
+int32_t type_room = 0;
+	int32_t S=0;
+
+	
+};
+*/
+//--------------BUILDING---------------
+
+
+
+void Building::print_data()
+{
+	std::cout << std::setw(10) << "Additional building: " << this->building_names[this->building_type] << " with ";
+
+	std::cout << (this->has_pipe_furnace ? "a " : "no ") << "furnace and a pipe.\n";
+	std::cout << "Area: " << this->S << "\n";
+}
+
+
+bool Building::check_data()
+{
+	if (this->building_type != 0 &&  this->building_type != 1 && this->building_type != 2) 
 	{
-		case BuildingType::bedroom : std::cout <<  "Bedroom";
-					     break;
-		case BuildingType::kitchen:	std::cout << "Kitchen";
-						break;
-		case BuildingType::bathroom: std::cout << "Bathroom";
-					     break;
-		case BuildingType::children_room: std::cout << "Children's room";
-						  break;
-		case BuildingType::dining_room: std::cout << "Dining room";
-						break;
-		default : std::cout << "Unknown building type.";
-			  break;
+		return 0;
 
 	}
-
+	if (this->S < 0)
+	{
+		return 0;
+	}
+	if(this->has_pipe_furnace != 1 && this->has_pipe_furnace != 0)
+	{
+		return 0;
+	}
+	return 1;
 }
+
+void Building::write_data()
+{
+	while(true)
+	{
+		std::cout << "Input buiding type number:\n";
+		for(size_t i=0; i<this->building_names.size(); ++i)
+		{
+		std::cout << std::setw(10) << std::left << this->building_names[i];
+		
+		std::cout << std::setw(10) << std::right << i;
+		std::cout << std::endl;
+		}
+
+		std::cin >> this->building_type;
+		std::cout << "Input whether the house hase furnace with a pipe. Yes(1) / No(0)\n";
+		std::cin >> this->has_pipe_furnace;
+		std::cout << "Input the size of the area:\n";
+		std::cin >> this->S;
+
+		if(this->check_data() && std:: cin)
+		{
+
+			break;
+		}
+		else
+		{
+			std::cout << "Invalid data. Try again.\n";
+			std::cin.ignore(1000,'\n');
+			std::cin.good();
+		}
+	}
+}
+double  Building:: evaluate_area()
+{
+return this->S;
+}
+
+
 
