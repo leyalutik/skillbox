183b45508951c68e17744c9c505d1d761cc6a755
leyalutik <ludapevek@gmail.com>
2024-10-22 00:26:28 +0300

    done_but_not_optimize

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX = g++
-CXXFLAGS = -ggdb -Wall -pthread -std=c++17
+CXXFLAGS = -ggdb -Wall -pthread -std=c++20
 SOURCE=main.cpp
 #MODULES=test_framework.cpp main_functions.cpp tic_tac_toe.cpp tests.cpp
 #OBJMODULES=$(MODULES:.cpp=.o)
@@ -17,7 +17,7 @@ run : $(GOAL)
 #	$(CXX) $(CXXFLAGS) -c $< 
 
 $(GOAL) : $(SOURCE) #$(OBJMODULES)
-	$(CXX) $(CXXFLAGS) $^ -o $@ 2>&1 error.log 
+	$(CXX) $(CXXFLAGS) $^ -o $@  
 
 clean : 
 	rm -f *.o
--- main.cpp
+++ main.cpp
@@ -1,262 +1,160 @@
 #include<string>
 #include<iostream>
-#include<vector>
-#include<ctime>
-#include<cstdlib>
-#include<fstream>
-#include<cctype>
-#include<sstream>
 #include<map>
-#include<set>
 
 enum class COMMAND
 {
-	ADD_NUMBER_SURNAME = 0,
-	FIND_SURNAME = 1,
-	FIND_NUMBER = 2
+
+ADD = 0,
+NEXT = 1
+
 };
 
-struct  Phone_book
+struct  Register
 {
+	Register();
+	void input(std::string& data); // call check_data and clear , define command
+	bool is_surname(const std::string& data);
+	bool is_next(const std::string& data);
+	void run_command();//add // next (output
+	bool check_data(const std::string& data)
+	{ return (is_surname(data) || is_next(data));}
+	std::map<std::string, int32_t> list;// surname, is_called (0 - all surnames are called, 1,2,3.. - surnames to be called)
+					    //	     void clear(); //if (is_called > list.size()/2) -> erase 
+	int32_t number_is_used = 0;
+	std::map<std::string , int32_t>::iterator current_iterator;
+	COMMAND command;
+	std::string current_surname;
 
-	COMMAND input_query(std::istream& is);
-	bool is_valid_number();
-	bool is_valid_surname();
-	bool is_number(const std::string& word);
-	bool is_surname(const std::string& word);
-
-	void run_command(COMMAND command);
-
-	std::map<std::string,std::string> numbers;
-	std::set<std::string> surnames;
-
-	std::string number;
-	std::string surname;
 };
-
-
 
 
 int main()
 {
-	bool exit = 1;
-	while(exit)
+
+	Register reg;
+	bool exit = false;
+	while(!exit)
 	{
 
-		std::cout << std::left <<std::setw(30)  <<  "Input a query:" << std::endl;
-		std::cout  << "-------------------------------\n";
-		std::cout << std::setw(30) << std::left <<  "Templates:" << std::endl;
-		std::cout  << std::setw(30)<< std::left << "<number>(xx-xx-xx) <surname>" << std::endl;
-		std::cout << std::setw(30) <<  std::left << "<number>(xx-xx-xx)" << std::endl;
-		std::cout << std::setw(30) <<  std::left << "<surname>" << std::endl;
-
-		std::cout << std::setw(32) << std::left << "--------------------------------------" << std::endl;
-
-		Phone_book phone_book;
-		phone_book.run_command(phone_book.input_query(std::cin));
-
-		std::cout << "Continue the program ? Yes(1)/ No(0):\n";
-		std::cin >> exit;
-		std::cin.clear();
-		std::cin.ignore(1000,'\n');
-
+		//display prompt
+		std::cout << "Input <Surname> or command <Next>:\n";
+		std::string data;
+		
+		reg.input(data);
+		reg.run_command();
+		if(reg.current_iterator == reg.list.end())
+		{
+			exit = true;
+		}
 	}
-
 	return 0;
 }
 
-//----------END---------MAIN--------------
+//----------------------------------
 
 
-COMMAND Phone_book::input_query(std::istream& is)
+Register::Register()
 {
-
-	COMMAND command = COMMAND::ADD_NUMBER_SURNAME;
-	int32_t LIMIT = 0;
-
-	while(LIMIT < 1000)
-	{
-		++LIMIT;
-		std::vector<std::string> words;
-		std::string line;
-		std::string word;
-		std::getline(std::cin, line, '\n');
-		std::stringstream ss(line);
-		while(ss >> word)
-		{
-			words.push_back(word);
-		}
-
-
-		if(words.size() ==0 || words.size() > 2)
-		{
-			std::cout << "Invalid number of words.Try again.\n";
-			continue;
-		}
-
-		if(is_number((words[0])) && (words.size() == 1))
-		{
-			this->number = words[0];
-			if(this->is_valid_number() && (words.size() == 1))
-			{
-				command = COMMAND::FIND_SURNAME;
-				break;
-			}
-		}
-		if(is_surname((words[0])))
-		{
-			this->surname = words[0];
-			if(this->is_valid_surname())
-			{
-				command = COMMAND::FIND_NUMBER;
-				break;
-
-			}
-		}
-
-		if(words.size() == 2)
-		{
-			this->number = words[0];
-			this->surname = words[1];
-		}
-		if(this->is_valid_number() && this->is_valid_surname())
-		{
-
-
-			command = COMMAND::ADD_NUMBER_SURNAME;
-			break;
-		}
-
-		std::cout << "Invalid number of words.Try again.\n";
-
-	}
-	if(LIMIT == 1000)
-	{
-		std::cout << "Multiplying invalid input.Program terminates.\n";
-	}
-	return command;
-
+	this->current_iterator = this->list.begin();
 }
-
-bool Phone_book::is_number(const std::string& word)
+void Register::run_command()
 {
-	if(word.size() == 0)
+	switch(this->command)
 	{
-		return 0;
-	}
-	return std::isdigit(word[0]);
-}
-
-bool Phone_book::is_surname(const std::string& word)
-{
-	if(word.size() == 0)
-	{
-		return 0;
-	}
-	return std::isalpha(static_cast<unsigned char>(word[0]));
-
-}
-
-
-bool Phone_book::is_valid_number()
-{
-	if(this->number.size() == 0 || this->number.size() != 8)
-	{
-		return 0;
-	}
-	for(size_t i=0; i<this->number.size(); ++i)
-	{
-		if(i == 2 || i ==5)
+		case COMMAND::ADD : 
 		{
-			if(this->number[i] != '-')
-			{
-				return 0;
-			}
-		}
-		else
-		{
-			if(!std::isdigit(this->number[i]))
-			{
-				return 0;
-			}
-
-		}
-	}
-		return 1;
-
-}
-
-bool Phone_book::is_valid_surname()
-{
-	if(this->surname.size() == 0)
-	{
-		return 0;
-	}
-	for(const auto& w : this->surname)
-	{
-		if(!std::isalpha(w))
-		{
-			return 0;
-		}
-	}
-
-	return 1;
-
-}
-
-void Phone_book::run_command(COMMAND command)
-{
-
-	switch(command)
-	{
-		case COMMAND::ADD_NUMBER_SURNAME :
-			{
-				std::cout << "Command:\nAdd a number and surname\n";
-				surnames.insert(this->surname);
-				numbers[this->number]=this->surname;
-			}
-			break;
-
-		case COMMAND::FIND_NUMBER :
-			{
-				std::cout << "Command:\nFind a number\n";
-				auto it = numbers.find(this->number);
-				if(it != numbers.end())
+				if(this->list.count(this->current_surname))
 				{
-
-					std::cout << "{" << it->first << ", " << it->second << "}\n"; 
+					++this->list[this->current_surname];
 				}
 				else
 				{
-					std::cout << "Numbers've not found.\n";
+
+					this->list[this->current_surname] = 1;
 				}
-			}
-			break;
-		case COMMAND::FIND_SURNAME :
-			{
-				std::cout << "Command:\nFind a surname\n";
-				if(surnames.count(this->surname))
-				{
-					for(auto it=numbers.begin(); it!=numbers.end(); ++it)
+
+				++this->number_is_used;
+				auto iterator = this->list.find(this->current_surname);
+
+				if(this->list.size() == 1 || current_iterator->first > iterator->first)
 					{
-						if(it->second == this->surname)
-						{
-							std::cout << "{" << it->first << ", " << it->second << "}\n"; 
-
-						}
+						current_iterator = iterator; //move up of list to display in lexigraphical order
 					}
-				}
-				else
-				{
-					std::cout << "Numbers've not found.\n";
-				}
+		}
+		break;
+		case COMMAND::NEXT : 
+		{
+			while(current_iterator != list.end() && current_iterator->second <=  0  ) // end of list or used surname
+			{
+				++current_iterator;
 			}
-			break;
-		default: std::cout << "Unknown command.\n";
-			 break;
+			if(current_iterator == this->list.end())
+			{
+				std::cout << "The end of list.\n";
+				return;
+			}
+			std::cout << current_iterator->first << "\n";
+			--current_iterator->second;//number of equal surnames
+		}
+		break;
+
+		default: 
+
+		std::cout << "Unknown command. \n";
+		break;
 	}
 
-	this->number.clear();
-	this->surname.clear();
+	if(this->number_is_used > this->list.size())
+{
+	std::erase_if(this->list,[](auto& list_elem)
+			{
+				return list_elem.second == 0;
+			}
+		     );
+			this->number_is_used = 0;
+
+
 }
 
+}
+
+
+void Register::input(std::string& data)
+{
+	std::cin >> data;
+	while(!check_data(data))
+	{
+		
+		std::cout << "Invalid data. Try again.\n";
+		data.clear();
+		std::cin.clear();
+		std::cin.ignore(1000,'\n');
+		std::cin >> data;
+	}
+	if(is_next(data))
+	{
+
+		command = COMMAND::NEXT;
+		return;
+	}
+	current_surname.clear();
+	current_surname = data;
+	command = COMMAND::ADD;
+}
+bool Register::is_next(const std::string& data)
+{
+	return ((data == "Next" ||   data == "next"));
+}
+
+bool Register::is_surname(const std::string& data)
+{
+	for(size_t i=0; i<data.size(); ++i)
+	{
+		if(!isalpha(data[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
