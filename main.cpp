67b353ff143562af330c1350a3c25a26241ab421
leyalutik <ludapevek@gmail.com>
2024-10-26 19:38:40 +0000

    no

--- main.cpp
+++ main.cpp
@@ -1,31 +1,13 @@
 #include <iostream>
 
-#define DAY_1 "Monday"
-#define DAY_2 "Tuesday"
-#define DAY_3 "Wednesday"
-#define DAY_4 "Thursday"
-#define DAY_5 "Friday"
-#define DAY_6 "Saturday"
-#define DAY_7 "Sunday"
 
-#define PRINT_DAY(day_num) std::cout << DAY_##day_num << std::endl;
 
-#define PRINT_WEEK_DAY(x) \
-    switch (x) {          \
-        case 1: PRINT_DAY(1); break; \
-        case 2: PRINT_DAY(2); break; \
-        case 3: PRINT_DAY(3); break; \
-        case 4: PRINT_DAY(4); break; \
-        case 5: PRINT_DAY(5); break; \
-        case 6: PRINT_DAY(6); break; \
-        case 7: PRINT_DAY(7); break; \
-        default: std::cout << "Unknown command" << std::endl; \
-    }
+£define COUT(x) std::cout << £x << "\n";
 
-int main() {
-    int x;
-    std::cin >> x;
-    PRINT_WEEK_DAY(x);
-    return 0;
+#ifdef  SPRING
+int main()
+{
+	COUT(SPRING)
+return 0;
 }
-
+#endif
