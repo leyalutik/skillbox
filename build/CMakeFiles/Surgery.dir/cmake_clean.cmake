3626e4a8534b417756e603071042faecf0577f71
leyalutik <ludapevek@gmail.com>
2024-11-07 11:06:43 +0300

    no

---
+++ build/CMakeFiles/Surgery.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Surgery.dir/main.cpp.o"
+  "CMakeFiles/Surgery.dir/main.cpp.o.d"
+  "Surgery"
+  "Surgery.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Surgery.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Surgery.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Surgery.
+# This may be replaced when dependencies are built.
